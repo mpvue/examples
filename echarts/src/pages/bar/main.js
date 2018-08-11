83e83875e4ca3f9ff30ec756233800e25dfae7ef
anchengjian <anchengjian@gmail.com>
2018-08-11 18:21:09 +0800

    chore: 更新一下最新的模版工程

--- echarts/src/pages/bar/main.js
+++ echarts/src/pages/bar/main.js
@@ -3,12 +3,3 @@ import App from './index'
 
 const app = new Vue(App)
 app.$mount()
-
-export default {
-  config: {
-    // 这儿添加要用的小程序组件
-    usingComponents: {
-      'ec-canvas': '../../../static/ec-canvas/ec-canvas'
-    }
-  }
-}
